8d548f3cf0a6b51abe5eea0ed5f4cb8fca41b634
GalaxyQueen <olivia@deliberty.net>
2017-05-14 22:49:20 -0700

    just to save it

--- scrath.py
+++ scrath.py
@@ -1,17 +1,33 @@
-def myFunction():
-    try:
-        userInput = input('Please insert integer: ')
-        integer = int(userInput)
-        check = integer % 2
+# def myFunction():
+#     try:
+#         userInput = input('Please insert integer: ')
+#         integer = int(userInput)
+#         check = integer % 2
+#
+#         if check == 0:
+#             print('Even number')
+#         elif check == 1:
+#             print('odd number')
+#         else:
+#             print('ERROR! ' + userInput)
+#     except:
+#         myFunction()
+#
+# myFunction()
 
-        if check == 0:
-            print('Even number')
-        elif check == 1:
-            print('odd number')
-        else:
-            print('ERROR! ' + userInput)
-    except:
-        myFunction()
+def newFunction():
+    leftInput = input('input1')
+    leftInteger = int(leftInput)
 
-
-myFunction()
+    rightInput = input('input2')
+    rightInteger = int(rightInput)
+    function = input('function')
+    if input('function')=# XXX:
+        print('input1'*'input2')
+    if input('function')=/
+        print('input1'%'input2')
+    if input('function')=+
+        print('input1'+'input2')
+    if input('function')=-
+        print('input1'-'input2')
+newFunction()
