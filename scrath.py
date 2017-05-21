c3d647a105c9512418550d3787e1597a749a0de1
Andrew Watson <adw@Andrews-MacBook-Air.local>
2017-05-21 15:00:31 -0700

    sunday changes

--- scrath.py
+++ scrath.py
@@ -1,33 +1,47 @@
-# def myFunction():
-#     try:
-#         userInput = input('Please insert integer: ')
-#         integer = int(userInput)
-#         check = integer % 2
-#
-#         if check == 0:
-#             print('Even number')
-#         elif check == 1:
-#             print('odd number')
-#         else:
-#             print('ERROR! ' + userInput)
-#     except:
-#         myFunction()
-#
-# myFunction()
 
-def newFunction():
-    leftInput = input('input1')
-    leftInteger = int(leftInput)
+def sumFunction(x):
+	terminal = x
+	listOfNum = range(1, x + 1)
+	total = 0
+	for y in listOfNum:
+		total = y + total
+	print(total)
 
-    rightInput = input('input2')
-    rightInteger = int(rightInput)
-    function = input('function')
-    if input('function')=# XXX:
-        print('input1'*'input2')
-    if input('function')=/
-        print('input1'%'input2')
-    if input('function')=+
-        print('input1'+'input2')
-    if input('function')=-
-        print('input1'-'input2')
-newFunction()
+sumFunction(1000000)
+
+
+def revSumFunction(x):
+	terminal = x
+	potatosList = range(x, 101)
+	total = 0
+	for potatos in potatosList:
+		total = potatos + total
+	print(total)
+
+revSumFunction(5)
+
+
+
+def saleCalculator(medicine, population, price, prevalance):
+	#at some point add time
+	sales= population * (prevalance / 100) * price
+	print(medicine +', $' + str(int(sales))+ ' revenue')
+	return(sales)
+
+def getUserInputs():
+	ammountOfUserInputs = int(input('How many medicines do you want to input?: '))
+	listOfMedicines = range(1, ammountOfUserInputs + 1)
+	total = 0
+	for medicineNumber in listOfMedicines:
+
+		medicine = input('What is this medicine named: ')
+		population = input('What is the total population: ')
+		price = input('What is the price?: ')
+		prevalance = input('What is the prevalance (in total percent?): ')
+		if int(prevalance) > 100:
+			prevalance = input('please, input a percent below 100%')
+		sales = saleCalculator(medicine, int(population), int(price), int(prevalance))
+		total = round(total + sales, 2)
+	print('total revenue is $' + str(total))
+
+# getUserInputs()
